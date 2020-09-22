9ace8ceb9bb04f948ad5de75aa39ecd0227fabc3
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-09-22 21:04:50 +0200

    61. Adding Markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './ Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -7,4 +10,16 @@ export class CustomMap {
       zoom: 1,
     });
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {}
 }

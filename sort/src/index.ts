59b09ce8c7467f68a0983ff9d09b5e597c21132b
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-09-24 21:22:34 +0200

    75. Sorting Implementation (Bubble Sort)

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,20 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
 }
 const sorter = new Sorter([4, 3, -1, 10, 8]);
 sorter.sort();

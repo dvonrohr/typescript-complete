8c8ea835ace841f6151b41ad599b3529e28fb02a
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-09-24 21:17:15 +0200

    74. Sorter Scaffolding

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+const sorter = new Sorter([4, 3, -1, 10, 8]);
+sorter.sort();
+console.log(sorter.collection);

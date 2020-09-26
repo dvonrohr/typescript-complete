18dcd26d0a407af03acb98731f40bc7c11f94eb9
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-09-26 20:00:26 +0200

    84. Sorting Arbitrary Collections

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -13,7 +13,7 @@ export class Sorter {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i; j++) {
+      for (let j = 0; j < length - i - 1; j++) {
         this.collection.compare(j, j + 1);
         this.collection.swap(j, j + 1);
       }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 2]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const sorterNumbers = new Sorter(numbersCollection);
+sorterNumbers.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('aXbDeF');
+const sorterCharacters = new Sorter(charactersCollection);
+sorterCharacters.sort();
+console.log(charactersCollection.data);

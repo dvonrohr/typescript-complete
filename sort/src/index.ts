df14bf99d9d37190ec6719e16c8a8784b4a7c90f
Daniel Rudolf von Rohr <d.rudolf.von.rohr@gmail.com>
2020-09-26 20:31:51 +0200

    85. Linked List Implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 2]);
 const sorterNumbers = new Sorter(numbersCollection);
@@ -11,3 +12,12 @@ const charactersCollection = new CharactersCollection('aXbDeF');
 const sorterCharacters = new Sorter(charactersCollection);
 sorterCharacters.sort();
 console.log(charactersCollection.data);
+
+const linkedListCollection = new LinkedList();
+linkedListCollection.add(500);
+linkedListCollection.add(1);
+linkedListCollection.add(-99);
+linkedListCollection.add(33);
+const sorterLinkedList = new Sorter(linkedListCollection);
+sorterLinkedList.sort();
+linkedListCollection.print();
